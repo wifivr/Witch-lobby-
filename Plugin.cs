48e11b69392e469049d3d05c41ed83033bfbbaed
macha <128656293+machalolxp@users.noreply.github.com>
2024-12-23 23:48:01 -0600

    add sc

---
+++ Plugin.cs
@@ -0,0 +1,50 @@
+﻿using BepInEx;
+using UnityEngine;
+using Photon.Pun;
+using System;
+
+[BepInPlugin("com.yourname.gorillatag.lobbylogger", "Lobby Logger", "1.0.0")]
+public class LobbyLogger : BaseUnityPlugin
+{
+    private GUIStyle guiStyle = new GUIStyle(); // Style for the text
+    private string currentLobbyCode = "Not connected"; // Store the current lobby code
+
+    void OnEnable()
+    {
+        // Hooking the event for when Gorilla Tag is loaded
+        Utilla.Events.GameInitialized += OnGameInitialized;
+    }
+
+    void OnDisable()
+    {
+        // Unhooking the event
+        Utilla.Events.GameInitialized -= OnGameInitialized;
+    }
+
+    void OnGameInitialized(object sender, EventArgs e)
+    {
+        // Additional initialization if needed
+    }
+
+    void Update()
+    {
+        // Update the current lobby code if connected
+        if (PhotonNetwork.InRoom)
+        {
+            currentLobbyCode = PhotonNetwork.CurrentRoom.Name;
+        }
+        else
+        {
+            currentLobbyCode = "Not connected";
+        }
+    }
+
+    void OnGUI()
+    {
+        guiStyle.fontSize = 24; // Change the font size
+        guiStyle.normal.textColor = Color.white; // Change the font color
+
+        // Display the current lobby code on the screen
+        GUI.Label(new Rect(10, 10, 300, 50), $"Room Code: {currentLobbyCode}", guiStyle);
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace lobbylog
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.wifi.gorillatag.lobbylog";
+        public const string Name = "lobbylog";
+        public const string Version = "1.0.0";
+    }
+}
